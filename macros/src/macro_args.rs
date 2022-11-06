b26818895a4eb0e2761436956b3d6dce12c829b0
Steve Cook <steviecook210@gmail.com>
2022-11-06 12:29:24 -0500

    Revert "Backtrace macro attribute"
    This reverts commit b09e478e25c51e76df4c7257d35ed2ac3bfcbd5b.
    
    # Conflicts:
    #	macros/src/endian_read.rs
    #	macros/src/endian_write.rs
    #	macros/src/macro_args.rs
    #	macros/tests/endian_read.rs

--- macros/src/macro_args.rs
+++ macros/src/macro_args.rs
@@ -3,8 +3,7 @@ use syn::Attribute;
 
 #[derive(Debug, FromMeta)]
 pub struct MacroArgs {
-    pub pad_before: Option<usize>,
-    pub backtrace: Option<usize>,
+    pub pad_before: usize,
 }
 
 impl MacroArgs {
